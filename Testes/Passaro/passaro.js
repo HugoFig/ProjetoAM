a9e1aba23f9a59dab1c82fead0448d9a82c6e36b
Hugo Figueiredo <105859710+HugoFig@users.noreply.github.com>
2022-06-09 17:57:41 +0100

    Adicção da pasta do jogo flappy bird funcional.

---
+++ Testes/Passaro/passaro.js
@@ -0,0 +1,51 @@
+// passaro.js
+
+const spritePassaro = new Image();
+spritePassaro.src = 'spritePassaro.png';
+
+class Passaro{
+    constructor(){
+        this.x = 150;
+        this.y = 200;
+        this.velocidadey = 0;
+        this.originalWidth = 929;
+        this.originalHeight = 635;
+        this.width = this.originalWidth/20;
+        this.height = this.originalHeight/20;
+        this.peso = 1;
+        this.framex = 0;
+    }
+    update(){
+
+        if(this.y > canvas.height - (this.height * 3)){
+            this.y = canvas.height - (this.height * 3) ;
+            this.velocidadey = 0;
+        } else{
+            this.velocidadey += this.peso;
+            this.velocidadey *= 0.9;
+            this.y += this.velocidadey;
+        }
+
+        if(this.y < 0 + this.height){
+            this.y = 0 + this.height;
+            this.velocidadey = 0;
+        }
+
+        if(spacePressed) this.flap()
+           
+    }
+    draw(){
+        ctx.fillStyle = 'red'
+        //ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(spritePassaro, this.framex * this.originalWidth, 0, this.originalWidth, this.originalHeight, 
+            this.x - 25, this.y - 12, this.width * 1.5, this.height * 1.5);
+    }
+    flap(){
+        this.velocidadey -= 2;
+        if(this.framex >= 7) this.framex = 0;
+        else if(frame%4 == 0) this.framex++; 
+    }
+}
+
+const passaro = new Passaro();
+
